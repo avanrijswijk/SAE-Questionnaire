bae95f1e34dd2a9b8261f341dd7129cdf980bf1f
Henzo RENET <henzodu33@gmail.com>
2025-12-01 16:18:22 +0100

    ajout de la page d'acceuil

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'header.php');
 
     // routage simple
-    $controller = isset($_GET['c'])? $_GET['c'] : 'acceuil';
+    $controller = isset($_GET['c'])? $_GET['c'] : 'home';
 
     switch ($controller) {
-        case 'acceuil':
-            //require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Controllers'.DIRECTORY_SEPARATOR.'homeController.php');
+        case 'home':
+            require_once(__DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Controllers'.DIRECTORY_SEPARATOR.'homeController.php');
             break;
     }
 
--- src/Views/header.php
+++ src/Views/header.php
@@ -5,12 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Page d'acceuille</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
-    <link rel="stylesheet" href="./css/style-header.css">
+    <link rel="stylesheet" href="./src/Views/css/style-header.css">
   </head>
   <body>
     <header id="header">
         <div id="div-header"></div>
         <img id="img-header" 
-             src="./img/unilim.png" 
+             src="./src/Views/img/unilim.png" 
              alt="logo unilim">
     </header>
