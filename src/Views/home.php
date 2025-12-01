764b071f6c70680543ced4f03496986ae529a7f0
Henzo RENET <henzodu33@gmail.com>
2025-12-01 17:23:31 +0100

    mise en place du css de la page d'accueil

--- src/Views/home.php
+++ src/Views/home.php
@@ -1,44 +1,31 @@
     <main>
-      <div>
-        <div>
-          <h1>Que voulez-vous faire ?</h1>
+      <div class="is-flex is-flex-direction-column ">
+        <div style="justify-items: center;">
+          <h1 class="title is-1 mt-5">Que voulez-vous faire ?</h1>
+        </div>
+        <div class="columns">
+          
+          <a class="column is-flex is-flex-direction-column is-align-items-center p-6">
+            <div class="image m-6 p-6" style="background-color: #F5A320; border-radius: 75px;">
+              <img src="./src/Views/img/creation_questionnaire.png" alt="icon creation de questionnaire">
+            </div>
+            <h2 class="title is-2" style="text-align: center;">Créer ou modifier un questionnaire</h2>
+          </a>
+
+          <a class="column is-flex is-flex-direction-column is-align-items-center p-6">
+            <div class="image m-6 p-6" style="background-color: #F5A320; border-radius: 75px;">
+              <img src="./src/Views/img/repondre_questionnaire.png" alt="icon répondre à un questionnaire">
+            </div>
+            <h2 class="title is-2" style="text-align: center;">Répondre à un questionnaire</h2>
+          </a>
+
+          <a class="column is-flex is-flex-direction-column is-align-items-center p-6">
+            <div class="image m-6 p-6" style="background-color: #F5A320; border-radius: 75px;">
+              <img src="./src/Views/img/creation_questionnaire.png" alt="icon creation de questionnaire">
+            </div>
+            <h2 class="title is-2" style="text-align: center;">Voir les résultats d'un questionnaire</h2>
+          </a>
         </div>
 
-        <div>
-          <div>
-            <!-- Simple SVG icon: new document with plus sign -->
-            <svg width="200" height="200" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Créer un nouveau document">
-              <rect x="0" y="0" width="256" height="256" rx="36" fill="#F5A320"/>
-
-              <!-- White document shape (rounded top-right corner) -->
-              <!-- Document bounds: centered, with a rounded top-right corner -->
-              <path fill="#FFFFFF" d="
-                M 76 64
-                H 156
-                A 16 16 0 0 1 172 80
-                V 176
-                A 16 16 0 0 1 156 192
-                H 76
-                A 16 16 0 0 1 60 176
-                V 80
-                A 16 16 0 0 1 76 64
-                Z
-              "/>
-
-              <!-- Plus sign in the top-right area of the document -->
-              <!-- Circle background for the plus (subtle badge) -->
-              <circle cx="160" cy="96" r="35" fill="#F5A320"/>
-
-              <!-- Plus symbol (cutout using orange to match bg for crisp shape) -->
-              <g fill="#FFFFFF">
-                <rect x="155" y="75" width="10" height="40" rx="2"></rect>
-                <rect x="140" y="90" width="40" height="10" rx="2"></rect>
-              </g>
-            </svg>
-
-          </div>
-          <h2>Créer ou modifier un questionnaire</h2>
-        </div>
-      
       </div>
     </main>
