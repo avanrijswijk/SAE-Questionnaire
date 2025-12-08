f716f0fac0405cf6caa8947fd69352da30569e7a
Henzo RENET <henzodu33@gmail.com>
2025-12-08 15:28:21 +0100

    Modification de l'url de la page des questionnaires

--- src/Controllers/findController.php
+++ src/Controllers/findController.php
@@ -1,3 +1,3 @@
 <?php
 
-require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'trouver-questionnaire.php');
+require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'trouver_questionnaire.php');
