718d3d3f664c7478b4812b7155ab5246161b053e
Henzo RENET <henzodu33@gmail.com>
2025-12-08 15:30:14 +0100

    merge

--- src/Views/footer.php
+++ src/Views/footer.php
@@ -3,13 +3,4 @@
     <p>Réalisé par Adam, Adrian et Henzo</p>
   </footer>
   </body>
-
-<footer class="footer">
-  <div class="content has-text-centered fixed-bottom">
-    <p>&copy; <?php echo date("Y"); ?> - <strong>IUT</strong></p>
-    <p>par  Adrian / Henzo / Adam
-  </div>
-</footer>
-
-
 </html>
