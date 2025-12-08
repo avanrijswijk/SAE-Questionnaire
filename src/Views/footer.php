7c016cf8cdb0d30cc24981370702eeeb6bc789f5
Adrian VAN RIJSWIJK <adrianlvr127@gmail.com>
2025-12-08 11:28:36 +0100

    ajout du model Database + footer

--- src/Views/footer.php
+++ src/Views/footer.php
@@ -1,2 +1,11 @@
   </body>
+
+<footer class="footer">
+  <div class="content has-text-centered fixed-bottom">
+    <p>&copy; <?php echo date("Y"); ?> - <strong>IUT</strong></p>
+    <p>par  Adrian / Henzo / Adam
+  </div>
+</footer>
+
+
 </html>
