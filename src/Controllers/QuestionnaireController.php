662ab667ce448c019310e267dc669dfe822728ca
Adrian VAN RIJSWIJK <adrianlvr127@gmail.com>
2026-01-05 14:12:25 +0100

    ajout méthode enregistrer un questionnaire

--- src/Controllers/QuestionnaireController.php
+++ src/Controllers/QuestionnaireController.php
@@ -26,6 +26,26 @@ class QuestionnaireController {
         require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'resultatsQuestionnaire.php');
     }
 
+    public function enregistrer() {
+        $id = isset($_POST['id']) ? $_POST['id'] : null;
+        $titre = isset($_POST['titre']) ? $_POST['titre'] : null;
+        $date_expiration = isset($_POST['date_expiration']) ? $_POST['date_expiration'] : null;
+        $id_createur = isset($_POST['id_createur']) ? $_POST['id_createur'] : null;
+        $code = isset($_POST['code']) ? $_POST['code'] : null;
+
+        if (isset($id)) {
+            $ajoutOk = $this->questionnaireModel->update($id, $titre, $date_expiration);
+        } else {
+            $ajoutOk = $this->questionnaireModel->createQuestionnaire($titre, $id_createur, $date_expiration, $code);
+        }
+
+        if ($ajoutOk) {
+            require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'Views'.DIRECTORY_SEPARATOR.'home.php');
+        } else {
+            echo 'Erreur lors de l\'enregistrement.';
+        }
+    }
+
     public function supprimer($id = null) {
         if ($id === null) {
             $id = isset($_GET['id']) ? $_GET['id'] : null;
