02a04a5331b4d168988657abc83509d5a3bd5c30
Henzo RENET <henzodu33@gmail.com>
2026-01-07 10:55:34 +0100

    Amélioration de l'affichage des résultats

--- src/Views/resultatsQuestionnaire.php
+++ src/Views/resultatsQuestionnaire.php
@@ -2,33 +2,55 @@
 $questionnaireBDD = new Questionnaire();
 $questionnaires = $questionnaireBDD->getQuestionnaireBy(["id_createur" => 1]);
 ?>
-<main style="background-color: #EFEFEF; height: 100%;">
+<main style="background-color: #EFEFEF; height: 100%; overflow-y: auto;">
     <script src="./src/Views/js/resultats_questionnaire/afficher/resultat.js"></script>
-    <div class="p-6">
-        <h3 class="is-capitalized title is-3 has-text-weight-semibold">liste des questionnaires</h3>
-        <div class="is-flex is-flex-direction-row is-justify-content-space-between p-2 mb-2">
-            <p>Titre du questionnaire</p>
-        </div>
-        <div id="questionnaires">
-            <?php if (count($questionnaires) < 1): ?>
+    <div class="pt-5">
+        <h3 class="is-capitalized title is-3 has-text-weight-semibold pl-5">liste des questionnaires</h3>
+        <div id="questionnaires" class="is-flex is-justify-content-center pt-5 pb-2">
+            <?php if (count($questionnaires) < 1) { ?>
                 <span><?php echo "Aucun questionnaire"; ?></span>
-            <?php endif ?>
+            <?php } else { ?>
+                <style>
+                    td {
+                        text-align: center;
+                    }
 
-            <?php foreach ($questionnaires as $questionnaire): ?>
-                <div class="is-flex is-flex-direction-row is-justify-content-space-between p-2 mb-2" style="border: 1px solid black; background-color: #ffffff; align-items:center;" onclick="alert('affichage ficitif');">
-                    <p style="color: black"><?php echo $questionnaire["titre"] ?></p>
-                    <div class="is-flex is-flex-direction-row" style="border-left: 1px solid;">
-                       <div class="image is-32x32 ml-1 mr-2" onclick="telechargerResultats();">
-                            <img src="./src/Views/img/telecharger-64.png" alt="icon de téléchargement" title="Telecharger">
-                        </div>
-                        <br>
-                        <div class="image is-32x32" onclick="alert('suppression ficitif');">
-                            <img src="./src/Views/img/poubelle-64.png" alt="icon de poubelle" title="Supprimer">
-                        </div> 
-                    </div>
-                    
-                </div>
-            <?php endforeach ?>
+                    td:first-child {
+                        text-align: left;
+                    }
+                </style>
+                <table class="table is-hoverable" style="width: 95%;">
+                    <colgroup>
+                            <col style="width:80%;">
+                            <col style="width:10%;">
+                            <col style="width:10%;">
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <th>Titre</th>
+                            <th>Telecharger</th>
+                            <th>Supprimer</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($questionnaires as $questionnaire): ?> 
+                            <tr onclick="event.stopPropagation();alert('affichage ficitif');">
+                                <td><?php echo $questionnaire["titre"] ?></td>
+                                <td>
+                                    <div class="image is-32x32 ml-1 mr-2" onclick="event.stopPropagation();alert('telechargement ficitif');">
+                                        <img src="./src/Views/img/telecharger-64.png" alt="icon de téléchargement" title="Telecharger">
+                                    </div>
+                                </td>
+                                <td>
+                                    <div class="image is-32x32" onclick="event.stopPropagation();alert('suppression ficitif');">
+                                        <img src="./src/Views/img/poubelle-64.png" alt="icon de poubelle" title="Supprimer">
+                                    </div> 
+                                </td> 
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            <?php } ?>
         </div>
     </div>
 </main>
