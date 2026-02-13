08f1828b29ef3cd9794f38dd917246ce35919e2d
Psychaine <adam.hobon@etu.unilim.fr>
2026-02-13 19:05:52 +0100

    ajout détection clic/doubleclic sur questionnaire

--- src/Views/js/resultats_questionnaire/afficher/resultat.js
+++ src/Views/js/resultats_questionnaire/afficher/resultat.js
@@ -1,3 +1,4 @@
+console.log("JS chargé !");
 /*function telechargerResultats(id) {
     const url = `?c=questionnaire&a=exporter&id=${id}`;
     const a = document.createElement('a');
@@ -6,3 +7,29 @@
     a.click();
     a.remove();
 }*/
+
+window.addEventListener('load', () => {
+
+    document.querySelectorAll('.ligne-questionnaire').forEach(tr => {
+        let timer = null;
+        const delay = 250;
+
+        // CLIC SIMPLE sur la ligne OU sur le texte
+        tr.addEventListener('click', e => {
+            timer = setTimeout(() => {
+                alert('clic simple');
+            }, delay);
+        });
+
+        // DOUBLE CLIC sur le texte du titre uniquement
+        const titreTexte = tr.querySelector('.titre-texte');
+        if (titreTexte) {
+            titreTexte.addEventListener('dblclick', e => {
+                e.stopPropagation();
+                clearTimeout(timer);
+                alert('double clic sur le texte');
+            });
+        }
+    });
+
+});
--- src/Views/resultatsQuestionnaire.php
+++ src/Views/resultatsQuestionnaire.php
@@ -8,7 +8,6 @@ $questionnaireBDD = new Questionnaire();
 $questionnaires = $questionnaireBDD->getQuestionnairePar(["id_createur" => 1]);
 ?>
 <main style="background-color: #EFEFEF; height: 100%; overflow-y: auto;">
-    <script src="./src/Views/js/resultats_questionnaire/afficher/resultat.js"></script>
     <div class="pt-5">
         <h3 class="is-capitalized title is-3 has-text-weight-semibold pl-5">liste des questionnaires</h3>
         <div id="questionnaires" class="is-flex is-justify-content-center pt-5 pb-2">
@@ -41,8 +40,10 @@ $questionnaires = $questionnaireBDD->getQuestionnairePar(["id_createur" => 1]);
                     </thead>
                     <tbody>
                         <?php foreach ($questionnaires as $questionnaire): ?> 
-                            <tr onclick="event.stopPropagation();alert('affichage ficitif');">
-                                <td><?php echo $questionnaire["titre"] ?></td>
+                            <tr class="ligne-questionnaire" data-id="<?php echo $questionnaire['id']; ?>">
+                                <td class="titre-questionnaire">
+                                    <span class="titre-texte"><?php echo $questionnaire["titre"] ?></span>
+                                </td>
                                 <td><?php //echo ($acceptesController->nombreReponduText($questionnaire["id"]))?></td> <!-- renvoi rien -->
                                 <td>
                                     <div class="image is-32x32 ml-1 mr-2" onclick="event.stopPropagation();alert('téléchargement en cours'); window.location.href = './?c=questionnaire&a=exporter&id=<?php echo $questionnaire['id']; ?>';">
@@ -62,3 +63,5 @@ $questionnaires = $questionnaireBDD->getQuestionnairePar(["id_createur" => 1]);
         </div>
     </div>
 </main>
+
+<script src="./src/Views/js/resultats_questionnaire/afficher/resultat.js"></script>
