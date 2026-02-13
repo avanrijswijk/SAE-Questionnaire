4739db5dae293d0962c7d67ae2f38b9fbb5a87dc
Psychaine <adam.hobon@etu.unilim.fr>
2026-02-13 10:31:33 +0100

    abandon de la branche

---
+++ src/Controllers/UtilisateursController.php
@@ -0,0 +1,31 @@
+<?php
+// un utilisateur n'exhiste pas dans la base de données, il est représenter par un code obtenue à partie des informations répucpérées.
+
+namespace App\Controllers;
+
+class UtilisateursController {
+
+    /**
+     * Constructeur de la classe QuestionController.
+     * Initialise les instances du modèle Question et du contrôleur Choix_possibleController.
+     */
+    public function __construct() {
+    }
+
+ function getCodeUtilisateur() {
+        $code = '';
+        //group
+        if (true) {
+            ;
+        } else {
+           ;
+        }
+        //identifiant
+        if (true) {
+            ;
+        } else {
+           ;
+        }
+        return $code;
+    }
+}
